42bc3cbabd6775922eb83302fb71a3fb743daf17
Muneeb <muneebkhalid98@gmail.com>
2025-04-22 11:38:22 +0500

    Resolved issues

--- Parser.py
+++ Parser.py
@@ -4,12 +4,12 @@ import re
 import json
 import logging
 
+from constants import email_pattern, phone_pattern, skills_keywords
+
+
 path = 'resume_samples.zip'
 extract = 'extracted_resumes'
 
-email_pattern = r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+'
-phone_pattern = r'(\+92-\d{3}-\d{7})|(0\d{3}-\d{7})'
-skills_keywords = ["Python", "Java", "C++", "SQL", "Django", "React","Communication", "JavaScript", "Machine Learning","Data Analysis","Node.js"]
 
 # Extracting zip file
 def extract_zip_folder():
@@ -30,9 +30,6 @@ def get_txt_files():
             if file.lower().endswith(".txt"):
                 full_path = os.path.join(root, file)
                 txt_files.append(full_path)
-    print("Valid resume files:")
-    for tf in txt_files:
-        print(tf)
     return txt_files
 
 def parse_files(txt_files):
@@ -56,12 +53,9 @@ def parse_files(txt_files):
                 if phone_match:
                     resume_info["phone"] = phone_match.group()
 
-                found_skills = []
-                for skill in skills_keywords:
-                    if skill.lower() in content.lower():
-                        found_skills.append(skill)
+                resume_info["skills"] = [skill for skill in skills_keywords if skill.lower() in content.lower()]
 
-                resume_info["skills"] = found_skills
+                resume_info["name"]=[]
 
                 lines = content.strip().splitlines()
                 for line in lines:
@@ -88,15 +82,6 @@ def save_json(data):
     except Exception as e:
         logging.error(f"Error saving JSON: {e}")
 
-#printing json file
-def print_json():
-    try:
-        with open("parsed_resumes.json", "r", encoding="utf-8") as file:
-            data_r = json.load(file)
-        for resume in data_r:
-            print(resume)
-    except Exception as e:
-        logging.error(f"Error reading or parsing JSON: {e}")
 
 
 def main():
@@ -104,6 +89,5 @@ def main():
     txt_files = get_txt_files()
     data = parse_files(txt_files)
     save_json(data)
-    print_json()
 
 main()
